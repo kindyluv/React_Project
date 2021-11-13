37f2441e7b766e690299c9971a6fa559fadca425
kindyluv <onyeukwuamara@gmail.com>
2021-11-13 17:05:13 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Laptop from "./views/Laptop"
+import './App.css'
+
 
 function App() {
+
+  let name = 'mojoyin'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Laptop name="Apple" color="gold" madeIn="China" button="red"/>
+      <Laptop name="Dell" color="black" madeIn="Lagos" button="green"/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
