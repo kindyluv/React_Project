c1c6fcbd2bbd7c8e416260e15f739445ae25c2a7
kindyluv <onyeukwuamara@gmail.com>
2021-11-21 10:18:40 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import Laptop from "./views/Laptop"
 import './App.css'
-
+import './ewa.css'
+import './ewaInput.css'
+import MainBody from "./views/MainBody"
+import RegistrationPage from "./views/RegistrationPage"
+import LoginPage from "./views/LoginPage"
+// import Auth from "./ewa/views/auth/Auth"
 
 function App() {
 
-  let name = 'mojoyin'
+  // let name = 'mojoyin'
 
   return (
     <div className="container">
-      <Laptop name="Apple" color="gold" madeIn="China" button="red"/>
-      <Laptop name="Dell" color="black" madeIn="Lagos" button="green"/>
+      {/* <Laptop name="Apple" color="gold" madeIn="China" button="red"/> */}
+      {/* <Laptop name="Dell" color="black" madeIn="Lagos" button="green"/> */}
+       <LoginPage/>
+      <MainBody/>
+      <RegistrationPage/> 
+      {/* <Auth/> */}
     </div>
   )
 }
