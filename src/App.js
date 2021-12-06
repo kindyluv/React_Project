2d4a2ef6f42cb671dcd337167678c6409d751f71
kindyluv <onyeukwuamara@gmail.com>
2021-12-06 09:59:45 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Laptop from "./views/Laptop"
 import './App.css'
-import './ewa.css'
-import './ewaInput.css'
+// import './ewa.css'
+// import './ewaInput.css'
+import '../src/ewa/components/auth/login/login.css'
+import '../src/ewa/components/auth/register/register.css'
 import MainBody from "./views/MainBody"
 import RegistrationPage from "./views/RegistrationPage"
 import LoginPage from "./views/LoginPage"
+import Auth from "./ewa/views/auth/Auth";
 // import Auth from "./ewa/views/auth/Auth"
 
 function App() {
@@ -16,10 +19,10 @@ function App() {
     <div className="container">
       {/* <Laptop name="Apple" color="gold" madeIn="China" button="red"/> */}
       {/* <Laptop name="Dell" color="black" madeIn="Lagos" button="green"/> */}
-       <LoginPage/>
-      <MainBody/>
-      <RegistrationPage/> 
-      {/* <Auth/> */}
+      {/* <LoginPage/>*/}
+      {/*<MainBody/>*/}
+      {/*<RegistrationPage/> */}
+       <Auth/>
     </div>
   )
 }
