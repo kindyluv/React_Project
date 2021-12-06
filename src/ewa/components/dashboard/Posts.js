77165bdf4ac52e127275c88feefddae6d3daf488
kindyluv <onyeukwuamara@gmail.com>
2021-12-06 16:43:59 +0100

    ....

--- src/ewa/components/dashboard/Posts.js
+++ src/ewa/components/dashboard/Posts.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Post from "./Post";
+import axios, {Axios} from "axios";
 
 const Posts = () => {
+    const [loading, setLoading] = useState(false)
+    const [posts, setPosts] = useState([])
+
+    const getPosts = ()=> {
+        return axios.get("https://jsonplaceholder.typicode.com/photos")
+    }
+
+    useEffect(() => {
+        getPosts()
+            .then(
+                (res) => {
+                    console.log(res)
+                    setPosts(res.data.slice(0,10))
+            })
+            .catch((e)=>{
+                console.log(e)
+            })
+            .finally(() => [
+                setLoading(false)
+            ])
+    }, [])
     return (
         <div>
             <Post/>
